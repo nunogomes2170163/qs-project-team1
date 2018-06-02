3a9718c17a653eff9e4c5659f7ad2cfd96e9afa4
Nuno Gomes <2170163@my.ipleiria.pt>
2018-06-02 14:53:10 +0100

    fix datatable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    let dataTable = null;
-
     const STATES = {
         LINKED_IN: 'linkedin',
         FACEBOOK: 'facebook',
@@ -32,36 +30,6 @@ $(document).ready(function() {
             fail: function(error) {
                 alert("An error occured fetching the filtered data");
             },
-        });
-    }
-
-    function refreshDataTable(contacts) {
-        if (dataTable) dataTable.destroy();
-        $('#contacts tbody tr').remove();
-        Object.keys(contacts).forEach((key) => {
-            const contact = contacts[key];
-            $('#contacts tbody').append(`<tr><td>${contact.Guid}</td><td>${contact.GivenName} ${contact.Surname}</td><td><a href=get_contact.php?guid=${contact.Guid}>Open</a></td></tr>`);
-        });
-        initializeDataTable();
-    }
-
-    function initializeDataTable() {
-        dataTable = $('#contacts').DataTable({
-            order: [[ 1, "asc" ]],
-            'columnDefs': [
-                {
-                    targets: [ 0, 1, 2 ],
-                    className: 'mdl-data-table__cell--non-numeric'
-                },
-                {
-                    'sortable': false,
-                    'targets': [0, 2]
-                },
-                {
-                    'searchable': false,
-                    'targets': [0, 2]
-                }
-            ]
         });
     }
 
@@ -164,6 +132,4 @@ $(document).ready(function() {
             },
         });
     });
-
-    initializeDataTable();
 });
