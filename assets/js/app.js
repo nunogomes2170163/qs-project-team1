3d3511ba422945baa17f2ad334b27ee5394a45ca
Nuno Gomes <2170163@my.ipleiria.pt>
2018-06-03 19:02:51 +0100

    keep state on filters

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+    const STATES = {
+        LINKED_IN: 'linkedin',
+        FACEBOOK: 'facebook',
+    };
+
+    const filterState = {
+        facebook: {
+            on: $('#FACEBOOK').is(':checked'),
+                source: 'facebook',
+            },
+        linkedin: {
+            on: $('#LINKED_IN').is(':checked'),
+                source: 'linkedin',
+            },
+    };
+
+    function setState(field, value) {
+        filterState[field].on = value;
+    }
+
     function updateList() {
         $.ajax({
             data: filterState,
@@ -11,6 +31,8 @@ $(document).ready(function() {
     }
 
     $('.filter-chooser').change(function() {
+        const isOn = $(this).is(':checked');
+        setState(STATES[$(this).attr('id')], isOn);
         updateList();
     });
 });
